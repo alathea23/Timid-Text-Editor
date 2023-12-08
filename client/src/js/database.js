f2481a3607554b977a921f486c5f7410d9ff5679
alathea23 <135459921+alathea23@users.noreply.github.com>
2023-12-07 22:37:45 -0800

    updating service worker and installation

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const JATEDb = await openDB('text', 1);
+  const tx = JATEDb.transaction('text', 'readwrite');
+  const write = tx.objectStore('text');
+  const request = write.put({ id: id, value: content });
+  const result = await request;
+  console.log('Data saved to the database', result.value);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+  const JATEDb = await openDB('todos', 1);
+  const tx = JATEDb.transaction('todos', 'readonly');
+  const content = tx.objectStore('todos');
+  const request = content.getAll();
+  const result = await request;
+  console.log('result.value', result.value);
+  return result;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,27 @@
-const butInstall = document.getElementById('buttonInstall');
+const btnInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    btnInstall.style.visibility = 'visible'; //need to update this??  check for function
+    textHeader.textContent = 'Click the button to install!';
+  
+    btnInstall.addEventListener('click', () => {
+      event.prompt();
+      btnInstall.setAttribute('disabled', true);
+      btnInstall.textContent = 'Installed!';
+    });
+  });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//butInstall.addEventListener('click', async () => {});
+//Do I nned to update with async function??
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    btnInstall.style.visibility = 'hidden';
+    textHeader.textContent = 'Successfully installed!';
+    console.log('👍', 'appinstalled', event);
+  });
+  
